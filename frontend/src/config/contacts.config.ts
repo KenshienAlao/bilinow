1096b60c05c8eeb68f443eb806c2d856f404e39a
kenshien <kenshienworkacc@gmail.com>
2026-07-24 21:50:28 +0800

    feat: contact config

---
+++ frontend/src/config/contacts.config.ts
@@ -0,0 +1,8 @@
+export const CONTACTS = {
+  gmail: "kenshienworkacc@gmail.com",
+  facebook: "https://www.facebook.com/KenshienAndres",
+  github: "https://github.com/KenshienAlao",
+  linkedin: "https://www.linkedin.com/in/KenshienAlao/",
+  phone: "+63 9929820394",
+  location: "Kasiglahan Village, Rodriguez, Rizal",
+};
