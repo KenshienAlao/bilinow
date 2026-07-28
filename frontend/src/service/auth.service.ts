0c6eeb7088794dcc2ada7832a57304abdb11d763
kenshien <kenshienworkacc@gmail.com>
2026-07-29 00:00:59 +0800

    refactor: update auth service to use validated signin and signup types

--- frontend/src/service/auth.service.ts
+++ frontend/src/service/auth.service.ts
@@ -1,15 +1,15 @@
 import { API_ROUTES } from "@/config/api-routes";
 import api from "@/lib/api";
 import { ApiReponse } from "@/lib/response";
-import { Signin, Signup } from "@/validation/auth.validation";
+import { SigninValidated, SignupValidated } from "@/validation/auth.validation";
 
 export const AuthService = {
-  signup: async (data: Signup): Promise<ApiReponse> => {
+  signup: async (data: SignupValidated): Promise<ApiReponse> => {
     const res = await api.post<ApiReponse>(API_ROUTES.AUTH.SIGNUP, data);
     return res.data;
   },
 
-  signin: async (data: Signin): Promise<ApiReponse> => {
+  signin: async (data: SigninValidated): Promise<ApiReponse> => {
     const res = await api.post<ApiReponse>(API_ROUTES.AUTH.SIGNIN, data);
     return res.data;
   },
